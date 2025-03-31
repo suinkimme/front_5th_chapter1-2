93dcd962850d164c6cc8bffa04bf69d7cd607b6e
Suin Kim <contact@suinkim.me>
2025-03-31 22:03:48 +0900

    feat: boolean, null, undefined 빈 문자열 반환 및 number type은 문자열로 변환

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,11 @@
 export function normalizeVNode(vNode) {
+  if (typeof vNode === "boolean" || vNode === null || vNode === undefined) {
+    return "";
+  }
+
+  if (typeof vNode === "number" || typeof vNode === "string") {
+    return `${vNode}`;
+  }
+
   return vNode;
 }
