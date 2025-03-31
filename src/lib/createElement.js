1282821c881a34ec7b0ecb0ec9a8e0b6b82a3a16
Suin Kim <contact@suinkim.me>
2025-04-01 00:59:10 +0900

    feat: 배열 입력에 대해 DocumentFragment 처리

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,29 @@
-import { addEvent } from "./eventManager";
+// import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  // vNode가 null, undefined, boolean 일 경우, 빈 텍스트 노드를 반환합니다.
+  if (typeof vNode === "boolean" || vNode === null || vNode === undefined) {
+    return document.createTextNode("");
+  }
 
-function updateAttributes($el, props) {}
+  // vNode가 문자열이나 숫자면 텍스트 노드를 생성하여 반환합니다.
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode);
+  }
+
+  // vNode가 배열이면 DocumentFragment를 생성하고 각 자식에 대해 createElement를 재귀 호출하여 추가합니다.
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    vNode.forEach((child) => fragment.appendChild(createElement(child)));
+    return fragment;
+  }
+
+  const element = document.createElement(vNode.type);
+  (vNode.children || []).forEach((child) => {
+    element.appendChild(createElement(child));
+  });
+
+  return element;
+}
+
+// function updateAttributes($el, props) {}
