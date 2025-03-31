fb351a7e8eb8f1920dbdb7d07b2139be2427c264
Suin Kim <contact@suinkim.me>
2025-03-31 19:46:19 +0900

    docs: createVNode 함수 리팩터링하고 싶은 것 작성

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,11 @@
+/**
+ * Refactor
+ * 1. Array.flat()을 Custom Flat으로 변경
+ * - 메모리와 CPU 자원을 많이 사용해 성능 저하의 우려가 있음
+ * - 구버전 브라우저 지원 X, 크로스 브라우징 이슈 발생 가능성 있음
+ */
+
 export function createVNode(type, props, ...children) {
-  return { type, props, children };
+  const vNode = { type, props, children: children.flat(Infinity) };
+  return vNode;
 }
