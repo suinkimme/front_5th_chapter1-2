711ce470ba160e2e44ff0522ae3a2476d93d70ab
Suin Kim <contact@suinkim.me>
2025-04-01 21:32:31 +0900

    feat: 이벤트 위임 및 이벤트 제거

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -7,23 +7,44 @@
  */
 
 const eventMap = new Map();
+let rootElement = null;
 
 // setupEventListeners를 이용해서 이벤트 함수를 가져와서 한 번에 root에 이벤트를 등록합니다.
 export function setupEventListeners(root) {
-  eventMap.forEach((_, eventType) => {
-    const handlerMap = eventMap.get(eventType);
-    root.addEventListener(eventType, (e) => {
-      let target = e.target;
-      while (target && target !== root.parentNode) {
-        if (handlerMap.has(target)) {
-          const handler = handlerMap.get(target);
-          handler(e);
-        }
-        target = target.parentNode;
-      }
+  // 새로운 root로 바꾸기 전 기존 root의 리서너를 모두 제거함
+  if (rootElement && rootElement !== root) {
+    eventMap.forEach((_, eventType) => {
+      console.log(eventType);
+      rootElement.removeEventListener(eventType, handleEvent);
     });
+  }
+
+  rootElement = root;
+  // 이벤트 위임
+  eventMap.forEach((_, eventType) => {
+    rootElement.addEventListener(eventType, handleEvent);
   });
 }
+
+/**
+ * Refactor
+ * 2. 버블링, 캡처링 제어와 연결되도록 코드를 수정해볼 수 있을 것 같음
+ */
+
+// 이벤트 위임과 제거에 사용되는 공용 이벤트 처리기
+const handleEvent = (e) => {
+  const { type, target } = e;
+  const handlerMap = eventMap.get(type);
+
+  let currentTarget = target;
+  while (currentTarget && currentTarget !== rootElement.parentNode) {
+    if (handlerMap.has(currentTarget)) {
+      const handler = handlerMap.get(currentTarget);
+      handler(e);
+    }
+    currentTarget = currentTarget.parentNode;
+  }
+};
 
 // addEvent를 통해 element에 대한 이벤트 함수를 저장합니다.
 export function addEvent(element, eventType, handler) {
@@ -36,4 +57,22 @@ export function addEvent(element, eventType, handler) {
 }
 
 // removeEvent를 통해 element에 대한 이벤트 함수를 삭제합니다.
-// export function removeEvent(element, eventType, handler) {}
+export function removeEvent(element, eventType) {
+  if (!eventMap.has(eventType)) {
+    return;
+  }
+
+  const handlerMap = eventMap.get(eventType);
+  if (handlerMap.has(element)) {
+    handlerMap.delete(element);
+  }
+
+  // 해당 이벤트 타입에 핸들러가 없을 경우에 이벤트 타입 자료 구조 자체를 삭제함
+  if (!handlerMap.size) {
+    eventMap.delete(eventType);
+
+    if (rootElement) {
+      rootElement.removeEventListener(eventType, handleEvent);
+    }
+  }
+}
