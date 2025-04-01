bee4bcc3444b7f3f88e636116b460dc77606ebd4
Suin Kim <contact@suinkim.me>
2025-04-01 16:09:13 +0900

    docs: 이벤트 매니저 작성 예정 함수 설명

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,8 @@
-export function setupEventListeners(root) {}
+// // setupEventListeners를 이용해서 이벤트 함수를 가져와서 한 번에 root에 이벤트를 등록합니다.
+// export function setupEventListeners(root) {}
 
-export function addEvent(element, eventType, handler) {}
+// // addEvent를 통해 element에 대한 이벤트 함수를 저장합니다.
+// export function addEvent(element, eventType, handler) {}
 
-export function removeEvent(element, eventType, handler) {}
+// // removeEvent를 통해 element에 대한 이벤트 함수를 삭제합니다.
+// export function removeEvent(element, eventType, handler) {}
